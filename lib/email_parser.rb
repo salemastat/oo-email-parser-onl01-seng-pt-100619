08692bc207b77576313cd31b8705044b5ebec972
Salem Asrat <salemasrat@gmail.com>
2019-11-08 01:32:17 +0000

    Automatically backed up by Learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -1,4 +1,4 @@
-class EmailParser
+class  EmailParser
 
   attr_accessor :emails
 
@@ -6,10 +6,13 @@ class EmailParser
     @emails = emails
   end
 
-  def parse
-    emails.split(/[,?\s]/).select{|email| email != ""}.uniq 
-  end
-
-end
+ 
+  def parse 
+    emails.split.collect do |address|
+    address.split(',') 
+    end
+    .flatten.uniq 
+  end 
+end 
   
   
