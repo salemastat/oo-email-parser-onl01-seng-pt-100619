73f12b7e90f0942beced14f5c635167b3e001af5
Salem Asrat <salemasrat@gmail.com>
2019-11-08 01:20:17 +0000

    Automatically backed up by Learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -7,6 +7,9 @@ class EmailParser
   end
 
   def parse
+    # split into an array using the regex of "possible comma followed by white space"
+    # select from the array only those emails that are not blank spaces
+    # select only those unique emails
     emails.split(/[,?\s]/).select{|email| email != ""}.uniq 
   end
 
